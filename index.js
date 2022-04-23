801dab2a51f18ed920f29a58511ead45c4a32ac6
Bùi Minh Đức <93396463+minhduc-bui1@users.noreply.github.com>
2022-04-23 23:20:22 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var masterExt = navigator.engmodeExtension || navigator.jrdExtension || navigator.kaiosExtension
-var propSet = {
-  'service.adb.tcp.port': 5555,
-  'ctl.stop': 'adbd',
-  'ctl.start': 'adbd'
-};
-for(var key in propSet) {
-  masterExt.setPropertyValue(key, propSet[key])
-}
